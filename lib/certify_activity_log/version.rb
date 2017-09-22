958cb2ed438d78bd23aa82a5c4160e8f3cbba703
Doug Price <dprice@fearless.tech>
2017-09-22 14:18:16 -0400

    HUB-830: virtually blank changelog.  bump the version to 1.0.0

--- lib/certify_activity_log/version.rb
+++ lib/certify_activity_log/version.rb
@@ -1,8 +1,8 @@
 module CertifyActivityLog
   module VERSION
-    MAJOR = 0
-    MINOR = 2
-    PATCH = 1
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
     PRE_RELEASE = "".freeze # e.g., "-beta"
 
     STRING = ([MAJOR, MINOR, PATCH].join('.') + PRE_RELEASE).freeze
